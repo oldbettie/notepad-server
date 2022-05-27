750837f61ab4ba6fc2ae2f48a8711161d038a44e
oldbettiegaming@gmail.com <oldbettiegaming@gmail.com>
2022-05-27 09:51:48 +0800

    bcryptjs

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { db } = require("../models");
 const User = db.models.User;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 const { db } = require("../models");
 const User = db.models.User;
